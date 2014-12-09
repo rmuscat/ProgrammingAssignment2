37b1ab5b339ae9fe6a34a9122ea447cce2784165
Robert Muscat <muscat_robert@hotmail.com>
2014-12-09 23:42:13 +0000

    First version, needs final check after changing vectors & MD5

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,39 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  mInverse <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    mInverse <- NULL
+  }
+  
+  get <- function() {
+    x
+  }
+  
+  setInverse <- function(inverse) {
+    mInverse <<- inverse
+  } 
+  
+  getInverse <- function() {
+    mInverse
+  }
+  list (set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    localInverse <- x$getInverse()
+    if (!is.null(localInverse)) {
+      message("Retrieving cached inverse...")
+      return(localInverse)
+    }
+    localMatrix <- x$get()
+    localInverse <- solve(localMatrix,...)
+    x$setInverse(localInverse)
+    localInverse
 }
